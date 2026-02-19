1e6f68b3860db18d74949f7fdba2d8314ec7e9fd
Whispersource <a3215442287@qq.com>
2026-02-18 20:40:07 -0500

    feat: initial PLT hook implementation

--- jni/main.cpp
+++ jni/main.cpp
@@ -5,3 +5,5 @@ void* hook_plt(const char* name) { ... }
 void* hook_plt(const char* name) { ... }
 // Update at 2026-02-09T18:40:07.924793
 void* hook_plt(const char* name) { ... }
+// Update at 2026-02-18T20:40:07.924793
+void* hook_plt(const char* name) { ... }
